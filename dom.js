94bc522bc9078304909f132de11f501064acc159
apesalaccia <apesalaccia@homtial.com>
2022-11-05 16:41:53 -0300

    update

--- dom.js
+++ dom.js
@@ -3,7 +3,6 @@ const btnCalculate = document.getElementById('btnResumen')
 const btnDelete = document.getElementById('btnBorrar')
 const category = document.getElementById('categoria')
 const totalBuy = document.getElementById('total')
-const form = document.forms.formu
 const price = 200
 const selectCate = {
     1: 0.2,
@@ -22,8 +21,8 @@ totalBuy.textContent = total
 }
 function Delete(e){
     e.preventDefault()
-totalBuy.textContent = ""
-category.textContent = ""
-CantOfTickets.textContent = ""
+totalBuy.textContent = ''
+category.textContent = ''
+CantOfTickets.textContent = ''
 
 }
