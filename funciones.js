cb744b5a4647fa98e324157d1a5b394d5f56d40b
apesalaccia <apesalaccia@homtial.com>
2022-11-28 07:32:44 -0300

    update

--- funciones.js
+++ funciones.js
@@ -1,8 +1,17 @@
+const box1 = document.getElementById('Estudiante')
+const box2 = document.getElementById('Trainee')
+const box3 = document.getElementById('Junior')
+const cards = document.getElementsByClassName('card')
+const Name= document.getElementById('inputNombre')
+const firstName= document.getElementById('inputApellido')
+const email = document.getElementById('inputCorreo')
 const CantOfTickets = document.getElementById('cantidadEnt')
 const btnCalculate = document.getElementById('btnResumen')
 const btnDelete = document.getElementById('btnBorrar')
 const category = document.getElementById('categoria')
 const totalBuy = document.getElementById('total')
+
+
 const price = 200
 const selectCate = {
     1: 0.2,
@@ -15,6 +24,7 @@ btnCalculate.addEventListener('click',calculate)
 btnDelete.addEventListener('click', Delete)
 
 
+
 function calculate(){
     if(CantOfTickets.value >= 1  ){
 let total = price * CantOfTickets.value
@@ -24,9 +34,8 @@ totalBuy.textContent = total
 CantOfTickets.style.border ="2px solid light grey"
 alert("compra realizada")
 }
-else {totalBuy.textContent ="" || alert("completa el campo")
+else {totalBuy.textContent ="" || alert("completa todos los campos")
 
-CantOfTickets.style.border = "2px solid red"
 }
 
 }
@@ -37,6 +46,37 @@ totalBuy.textContent = ''
 category.value = ""
 CantOfTickets.value = ''
 CantOfTickets.style.border ="1px solid light grey"
-
+ Name.value = ""
+firstName.value = ""
+email.value = ""
 }
+
+function handleClick(box) {
+    box.ariaChecked = true
+    box.classList.replace('bg-transparent','bg-light')
+    category.value=(box.id==="Estudiante" && "1") ||
+    (box.id ==="Trainee" && "2") ||
+    (box.id ==="Junior" && "3")
+    for (let item of cards) {
+        if (item.id !== box.id) {       
+            item.classList.replace("bg-light","bg-transparent")
+            item.ariaChecked = false
+        }
+    }
+    }
+
+
+
+
+function assignEvents(box){
+    box.addEventListener("click", () => handleClick(box))
+}
+assignEvents(Estudiante)
+assignEvents(Trainee)
+assignEvents(Junior)
+
+
+
+
+
 
